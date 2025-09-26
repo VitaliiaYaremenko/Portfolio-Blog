4d3280d4730d8d6244f63b3b7f95d51bc3ba65e8
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2025-09-26 23:20:16 +0200

    feat: apply styles

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -12,4 +12,22 @@ document.addEventListener('DOMContentLoaded', () => {
       item.classList.add('categories__item--active');
     });
   });
+
+  const slides = Array.from(document.querySelectorAll('.testimonials__item'));
+
+  const prevBtn = document.querySelector('.testimonials__button--prev');
+  const nextBtn = document.querySelector('.testimonials__button--next');
+
+  let currentSlideIndex = slides.findIndex((slide) => slide.classList.contains('testimonials__item--active'));
+
+  if (currentSlideIndex === -1) currentSlideIndex = 0;
+
+  function showSlide(newIndex) {
+    slides[currentSlideIndex].classList.remove('testimonials__item--active');
+    currentSlideIndex = (newIndex + slides.length) % slides.length;
+    slides[currentSlideIndex].classList.add('testimonials__item--active');
+  }
+
+  prevBtn.addEventListener('click', () => showSlide(currentSlideIndex - 1));
+  nextBtn.addEventListener('click', () => showSlide(currentSlideIndex + 1));
 });
